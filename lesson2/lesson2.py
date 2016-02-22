fd05e148b0c90f97c855634ef81840323f569547
moonreader <holy_nova@126.com>
2016-02-23 05:35:17 +0800

    增加注册验证、存入数据库、用户展示页

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -3,20 +3,27 @@ import cgi
 import webapp2
 import string
 import jinja2
+import re
+from google.appengine.ex import db
+
+
 template_dir = os.path.join(os.path.abspath('.'),'htmls')
 env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape = True)
 
 # def render(template,**kw):
 # 	t = env.get_template(template)
+class User(db.modle):
+    username = db.StringProperty()
+    password = db.StringProperty()
+    mail = db.StringProperty()
+    time = db.DateTimeProperty(auto_now_add = True)
+
 
 class BaseClass(webapp2.RequestHandler):
 	def render(self, template,**kw):
 		t = env.get_template(template)
 		self.response.write(t.render(**kw))
 
-# class TestHandler(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.out.write('test hello world')
 
 class MainPage(BaseClass):
     def get(self):
@@ -26,23 +33,65 @@ class MainPage(BaseClass):
 class SignUp(BaseClass):
     def get(self):
     	self.render('sign_up.html')
+    # def verify(self,username,password,password_again,mail):
+
+
     def post(self):
     	username = self.request.get('username')
     	password = self.request.get('password')
     	password_again = self.request.get('password_again')
     	mail = self.request.get('mail')
     	# para = dict(us)
-    	params = dict(username = username,
-    					password = password,
-    					password_again = password_again,
+
+        #verify
+        flag_error = False
+        if not re.compile(r"^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$").match(username):
+            error_username = 'username is not avaliable'
+            flag_error = True
+        if password != password_again:
+            error_password_again = 'password does not match the first one'
+            flag_error = True
+        if not re.compile(r"^[a-z]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$").match(mail):
+            error_mail = 'wrong e-mail'
+            flag_error = True
+
+        if not flag_error:
+            new_user = User(name = name,
+                            password = password,
+                            mail = mail)
+            new_user.push()
+
+        params = dict(username = username,
+                        password = password,
+                        password_again = password_again,
                       mail = mail,
-                      error_username = username,
-                      error_mail = mail)
+                      error_username = error_username,
+                      error_password_again = error_password_again,
+                      error_mail = error_mail,
+                      success_msg = 'congratulations '+username+" successfully sign up")
+
+
+        
     	# t = env.get_template('sign_up.html')
     	# self.write(t.render(username = username,password = password,password_again = password_again,mail=mail))
     	self.render('sign_up.html',**params)
 	# def verify(self,username = '',password):
-		# pass 	
+		# pass 
+
+class SignIn(BaseClass):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+
+class Password(BaseClass):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+
 
 
 
@@ -50,4 +99,6 @@ class SignUp(BaseClass):
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sign_up',SignUp),
+    ('/signin',SignIn),
+    ('/password',PassWord),
 ], debug=True)
